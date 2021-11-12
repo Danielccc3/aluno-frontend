6ecd70624655a71fb77e5d1570931ebc90f9357a
Daniel Correa <Daniel.Correia@modalgr.com.br>
2021-11-12 20:36:40 -0300

    Deploy para o Heroku

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
-
-const api = axios.create ({
-    baseURL: 'http://localhost:5555'
+ 
+const api = axios.create({
+    baseURL: 'https://alunos-back-dcorrea.herokuapp.com'
 })
-
+ 
 export default api;
